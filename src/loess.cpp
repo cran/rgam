24990a766673edb207dcfc6897807a4f08e7b83c
Davor Cubranic <cubranic@stat.ubc.ca>
2012-04-04 00:00:00 +0000

    version 0.6.1

--- src/loess.cpp
+++ src/loess.cpp
@@ -1,21 +1,15 @@
-#include <Rmath.h>
-#include <R.h>
-
-#include <vector>
-
 #include "loess.h"
 
 using namespace arma;
-using std::vector;
 
-typedef union {void *p; DL_FUNC fn;} fn_ptr;
-
-DL_FUNC R_ExternalPtrAddrFn(SEXP s){
-   fn_ptr tmp;
-   tmp.p =  EXTPTR_PTR(s);
-   return tmp.fn;
+extern "C" {
+void loess_raw(double *y, double *x, double *weights, double *robust, Sint *d,
+               Sint *n, double *span, Sint *degree, Sint *nonparametric,
+               Sint *drop_square, Sint *sum_drop_sqr, double *cell,
+               char **surf_stat, double *surface, Sint *parameter,
+               Sint *a, double *xi, double *vert, double *vval, double *diagonal,
+               double *trL, double *one_delta, double *two_delta, Sint *setLf);
 }
-
 
 vec loess_fit(vec const& y, vec const& x, vec const& weights, const double span_) {
   int n = y.n_elem;
@@ -40,28 +34,9 @@ vec loess_fit(vec const& y, vec const& x, vec const& weights, const double span_
   double trL, one_delta, two_delta;
   int setLf = 0;
 
-  static void (*loess_raw)(double *y, double *x, double *weights, double *robust, Sint *d,
-                           Sint *n, double *span, Sint *degree, Sint *nonparametric,
-                           Sint *drop_square, Sint *sum_drop_sqr, double *cell,
-                           char **surf_stat, double *surface, Sint *parameter,
-                           Sint *a, double *xi, double *vert, double *vval, double *diagonal,
-                           double *trL, double *one_delta, double *two_delta, Sint *setLf) = NULL;
-  if (!loess_raw) {
-    Rcpp::Function getNativeSymbolInfo("getNativeSymbolInfo");
-    Rcpp::List nativeSymbolInfo = getNativeSymbolInfo("loess_raw");
-    SEXP xp = nativeSymbolInfo["address"];
-    DL_FUNC loess_raw_p = R_ExternalPtrAddrFn( xp ) ;
-    
-    loess_raw = (void(*)(double *, double *, double *, double *, Sint *,
-                         Sint *, double *, Sint *, Sint *,
-                         Sint *, Sint *, double *,
-                         char **, double *, Sint *,
-                         Sint *, double *, double *, double *, double *,
-                         double *, double *, double *, Sint *)) loess_raw_p;
-  }
 
-  (*loess_raw)(const_cast<double *>(y.begin()),
-               const_cast<double *>(x.begin()),
+  loess_raw(const_cast<double *>(y.begin()),
+            const_cast<double *>(x.begin()),
                const_cast<double *>(weights.begin()),
                const_cast<double *>(weights.begin()),
                &d, &n, &span,
